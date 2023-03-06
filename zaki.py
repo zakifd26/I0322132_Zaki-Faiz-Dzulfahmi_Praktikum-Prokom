aa825e4de10eb209dfd5aa5f6ff8e28326b5fbb5
zaki <zakifaiz26@student.uns.ac.id>
2023-03-06 10:34:42 +0700

    Commit_01

---
+++ zaki.py
@@ -0,0 +1,13 @@
+nama_depan = "zaki"
+nama_blk = "faiz"
+
+angka = 10
+desimal = 0,5
+boolean = True
+kelas_D = ["zaki", "kls d", "dll"]
+dictionary = ("nama :" "zaki", "kelas :" "KLS D")
+
+print(nama_depan)
+print("nama saya adalah zaki")
+print(f"nama saya adalah (nama)")
+print("nama saya zaki")
