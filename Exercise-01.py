0ddbf5cdab8861eac3cf103aff65575d09ce8a11
zakifd26 <zakifaiz26@student.uns.ac.id>
2023-03-10 09:41:07 +0700

    Exercise-

---
+++ Exercise-01.py
@@ -0,0 +1,16 @@
+#input
+nama = 'Zaki Faiz Dzulfahmi'
+nim = 'I0322132'
+alamat = 'Mendungan, Pabelan, Kartasura, Sukoharjo, Jawa Tengah'
+visualisasi = 'terkenal akan budayanya. Baik makanan, keindahan tata ruang kotanya serta banyak terdapat tempat wisata yang ada. Salah satu budaya yang menjadi ikonik kota solo adalah batik, banyak sekali pengrajin batik di kota solo.  '
+harapan = 'semoga saya bisa bekerja di perusahaan-perusahaan seperti yamaha, honda, toyota ataupun perusahaan ternama lainnya.'
+
+dict = {'a' : nama, 'b' : nim, 'c' : alamat, 'd' : visualisasi, 'e' : harapan}
+
+
+#hasil
+print("Nama saya adalah", dict['a'])
+print("NIM saya adalah", dict['b'])
+print("Saya tinggal di", dict['c'])
+print("Kota solo adalah kota yang", dict['d'])
+print("Harapan saya sebagai lulusan Teknik Industri adalah", dict['e'])
